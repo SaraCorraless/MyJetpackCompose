15f49b77cec93b3bb1008abe76969be06413135a
Sara <71656774+SaraCorraless@users.noreply.github.com>
2022-11-06 18:59:23 +0100

    design profile

--- app/src/main/java/com/cys/myjetpackcompose/screens/Profile.kt
+++ app/src/main/java/com/cys/myjetpackcompose/screens/Profile.kt
@@ -1,18 +1,20 @@
 package com.cys.myjetpackcompose.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import android.widget.Toast
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.cys.myjetpackcompose.R
 import com.cys.myjetpackcompose.navigation.AppNavigation
 import com.cys.myjetpackcompose.navigation.AppScreens
 
@@ -20,24 +22,53 @@ import com.cys.myjetpackcompose.navigation.AppScreens
 @Composable
 fun ProfileScreem(navController: NavController){
     Scaffold {
+        TopAppBar() {
+            Text(text = "Profile")
+        }
         BodyContentProfile(navController)
     }
 }
 
 @Composable
 fun BodyContentProfile(navController: NavController){
+
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .width(2000.dp)
+
+
     ) {
-        Text(text = "Perfil de Sara")
-        Button(onClick = {
-            navController.navigate(route =  AppScreens.SkillsScreen.route)
-        }) {
-            Text(text = "Perfil")
+
+        Image(painter = painterResource(id = R.drawable.fotoperfilgit1),
+            contentDescription = "foto",
+
+            Modifier
+                .border(
+                    border = BorderStroke(3.dp, Color.Black),
+                    shape = RoundedCornerShape(50)
+                )
+                .clip(RoundedCornerShape(size = 200.dp))
+                .size(250.dp)
+
+
+        )
+        MaterialTheme{
+            Text(text = "Sara Corrales Santos",
+                style = MaterialTheme.typography.h4)
         }
+        Button(onClick = {
+            navController.navigate(route = AppScreens.SkillsScreen.route)
+        },
+        Modifier.padding(top = 100.dp)
+            ) {
+            Text(text = "Skills")
+        }
+
+
     }
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/cys/myjetpackcompose/screens/Skills.kt
+++ app/src/main/java/com/cys/myjetpackcompose/screens/Skills.kt
@@ -1,15 +1,17 @@
 package com.cys.myjetpackcompose.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.cys.myjetpackcompose.navigation.AppNavigation
 import com.cys.myjetpackcompose.navigation.AppScreens
@@ -17,12 +19,22 @@ import com.cys.myjetpackcompose.navigation.AppScreens
 @Composable
 fun SkillsScreem(navController: NavController){
     Scaffold {
+        TopAppBar {
+
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Arrow back",
+            modifier = Modifier.clickable {
+                navController.popBackStack()
+            })
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(text = "Skills")
+        }
         BodyContentSkills(navController)
     }
 }
 
 @Composable
 fun BodyContentSkills(navController: NavController){
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
